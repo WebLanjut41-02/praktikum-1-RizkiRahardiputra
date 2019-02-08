3b4446b10798e0495fc497acc405bb1867cbef17
RizkiRahardiputra <42662883+RizkiRahardiputra@users.noreply.github.com>
2019-02-08 10:59:18 +0700

    Add files via upload

---
+++ views/halaman.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>View Halaman</title>
+</head>
+<body>
+    <?php
+    foreach ($kiriman as $kirim)
+    {
+        echo $kirim."<br>";
+    }
+    ?>
+
+</body>
+</html>
